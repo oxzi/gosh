e2c7f6a7c8eb48d25dcf14f9a79bcff3edf13e0e
Alvar <post@0x21.biz>
2019-08-05 23:25:26 +0200

    Helper functions for byte sizes

--- util.go
+++ util.go
@@ -3,18 +3,20 @@ package gosh
 import (
 	"errors"
 	"fmt"
+	"math"
 	"regexp"
 	"strconv"
 	"strings"
 	"time"
 )
 
+// This file contains utility functions for both time.Duration and byte sizes.
+
 // Time determination beyond days resp. weeks is a more complex issue. Due to
 // months of different lengths plus additional leap years and leap seconds,
 // there is no clear duration of a month. At this point the monthly average of
 // the Gregorian calendar was used.
 // Souce: https://www.quora.com/What-is-the-average-number-of-days-in-a-month
-
 const (
 	timeDay   time.Duration = 24 * time.Hour
 	timeWeek  time.Duration = 7 * timeDay
@@ -107,4 +109,70 @@ func PrettyDuration(d time.Duration) string {
 	}
 
 	return strings.TrimRight(b.String(), " ")
+}
+
+var (
+	bytePrefixes = []string{"B", "K", "M", "G", "T", "P"}
+
+	bytePattern = regexp.MustCompile(`\A(?P<size>\d+)(?P<unit>([KMGTP]i?)?B)\z`)
+)
+
+// ParseBytesize parses a positive, human readable and whole byte amount in the
+// binary prefix notation. Legit values might be "1B", "23KiB"/"23KB" etc.
+func ParseBytesize(s string) (size int64, err error) {
+	if !bytePattern.MatchString(s) {
+		err = ErrNoMatch
+		return
+	}
+
+	var unit string
+
+	parts := bytePattern.FindStringSubmatch(s)
+	for i, elemKey := range bytePattern.SubexpNames() {
+		switch elemKey {
+		case "size":
+			if sizeParse, sizeErr := strconv.Atoi(parts[i]); sizeErr != nil {
+				err = sizeErr
+				return
+			} else {
+				size = int64(sizeParse)
+			}
+
+		case "unit":
+			unit = parts[i][:1]
+		}
+	}
+
+	if size == 0 || unit == "" {
+		err = fmt.Errorf("Not all values were found, size := %d, unit := %s", size, unit)
+		return
+	}
+
+	for _, pref := range bytePrefixes {
+		if pref == unit {
+			break
+		}
+		size *= 1024
+	}
+
+	return
+}
+
+// PrettyBytesize returns a human readable representation of a byte size.
+func PrettyBytesize(bs int64) string {
+	for i, pref := range bytePrefixes {
+		if bs >= int64(math.Pow(1024, float64(i+1))) {
+			continue
+		}
+
+		relSize := float64(bs) / math.Pow(1024, float64(i))
+		if i == 0 {
+			return fmt.Sprintf("%.1f B", relSize)
+		} else {
+			return fmt.Sprintf("%.1f %siB", relSize, pref)
+		}
+	}
+
+	// It's a feature, okay?!
+	return "too big to fail"
 }
--- util_test.go
+++ util_test.go
@@ -1,6 +1,7 @@
 package gosh
 
 import (
+	"math"
 	"testing"
 	"time"
 )
@@ -59,6 +60,57 @@ func TestPrettyDuration(t *testing.T) {
 	for _, test := range tests {
 		if pretty := PrettyDuration(test.input); pretty != test.output {
 			t.Fatalf("%v resulted in %s instead of %s", test.input, pretty, test.output)
+		}
+	}
+}
+
+func TestParseBytesize(t *testing.T) {
+	tests := []struct {
+		input  string
+		output int64
+		valid  bool
+	}{
+		{"1B", 1, true},
+		{"1MB", 1024 * 1024, true},
+		{"1MiB", 1024 * 1024, true},
+		{"23KiB", 23 * 1024, true},
+		{"1Mi", 0, false},
+		{"0B", 0, false},
+		{"", 0, false},
+	}
+
+	for _, test := range tests {
+		bs, err := ParseBytesize(test.input)
+		if (err == nil) != test.valid {
+			t.Fatalf("Error: %v, expected %t", err, test.valid)
+		}
+
+		if !test.valid {
+			continue
+		}
+
+		if bs != test.output {
+			t.Fatalf("Size mismatches, expected %d and got %d", test.output, bs)
+		}
+	}
+}
+
+func TestPrettyBytesize(t *testing.T) {
+	tests := []struct {
+		input  int64
+		output string
+	}{
+		{1, "1.0 B"},
+		{1024, "1.0 KiB"},
+		{1536, "1.5 KiB"},
+		{1536 * 1024, "1.5 MiB"},
+		{1536 * 1024 * 1024, "1.5 GiB"},
+		{math.MaxInt64, "too big to fail"},
+	}
+
+	for _, test := range tests {
+		if pretty := PrettyBytesize(test.input); pretty != test.output {
+			t.Fatalf("%d resulted in %s instead of %s", test.input, pretty, test.output)
 		}
 	}
 }
