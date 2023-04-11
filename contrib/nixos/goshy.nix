75a36242de42fbeaa33f65a30fe22ac647670c52
Lennart Mühlenmeier <lennart@cope.cool>
2023-04-11 20:27:15 +0200

    Rework nixos program after review by oxzi

--- contrib/nixos/goshy.nix
+++ contrib/nixos/goshy.nix
@@ -4,70 +4,22 @@ with lib;
 let
   cfg = config.programs.goshy;
 
-  goshy = pkgs.writers.writeBashBin "goshy" ''
-    show_help() {
-      cat <<EOF
-    Usage: goshy [Options] FILE
-    Options:
-      -b --burn              Burn after reading.
-      -h --help              Print this help message.
-      -u --only-url          Print only URL as response.
-      -p --period TIMEFRAME  Set a custom expiry period.
-    EOF
-    }
+  goshy = pkgs.stdenv.mkDerivation rec {
+    name = "goshy";
+    src = ../../.;
+    buildInputs = [ pkgs.bash pkgs.curl ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    installPhase = ''
+      mkdir -p $out/bin
+      cp ./contrib/bash/goshy $out/bin/goshy
+      wrapProgram $out/bin/goshy \
+        --set GOSH_INSTANCE ${cfg.instance} \
+        --set PERIOD "-F time=${cfg.expiryPeriod}" \
+        ${lib.optionalString cfg.burnAfterReading "--set BURN '-F burn=1'"} \
+        ${lib.optionalString cfg.printOnlyUrl "--set ONLYURL '?onlyURL'"} \
+    '';
+  };
 
-    while getopts p:period: flag
-    do
-      case "$\{flag\}" in
-        p) PERIOD_IS_SET=1;;
-        period) PERIOD_IS_SET=1;;
-      esac
-    done > /dev/null 2>&1
-    
-    if [ -z $PERIOD_IS_SET ]; then
-      ${lib.optionalString (cfg.defaults.expiryPeriod != null) "PERIOD='${cfg.defaults.expiryPeriod}'"}
-    fi
-
-    ${lib.optionalString cfg.defaults.burnAfterReading "BURN='-F burn=1'"} \
-    ${lib.optionalString cfg.defaults.printOnlyUrl "ONLYURL='?onlyURL'"} \
-
-    while :; do
-      case "$1" in
-        -p|--period)
-          PERIOD=$2
-          shift
-          ;;
-
-        -b|--burn)
-          BURN='-F burn=1'
-          ;;
-
-        -u|--only-url)
-          ONLYURL='?onlyURL'
-          ;;
-
-        -h|--help)
-          show_help
-          exit 0
-          ;;
-
-        *)
-          FILE=$1
-          break
-      esac
-
-      shift
-    done
-
-    PERIOD="-F period=$PERIOD"
-
-    if [[ -z "$1" ]]; then
-      show_help
-      exit 1
-    fi
-
-    ${pkgs.curl}/bin/curl ${cfg.instance}$ONLYURL -F "file=@$1" $PERIOD $BURN 
-  '';
 
 in {
   options = {
@@ -76,15 +28,13 @@ in {
       instance = mkOption {
         type = types.str;
       };
-      defaults = {
-        burnAfterReading = mkEnableOption "Enable burn after reading by default";
-        expiryPeriod = mkOption {
-          type = types.nullOr types.str;
-          description = "Set a custom default expiry period, e.g., one minute";
-          default = "3d";
-        };
-        printOnlyUrl = mkEnableOption "Enable only printing the resulting URL";
+      burnAfterReading = mkEnableOption "Enable burn after reading by default";
+      expiryPeriod = mkOption {
+        type = types.nullOr types.str;
+        description = "Set a custom default expiry period, e.g., one minute";
+        default = "3d";
       };
+      printOnlyUrl = mkEnableOption "Enable only printing the resulting URL";
     };
   };
 
