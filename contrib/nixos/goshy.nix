e8ac4f2fccec31f9eb9be7e802b9586d4f24a805
Alvar <post@0x21.biz>
2023-05-22 20:27:14 +0200

    goshy fixups

--- contrib/nixos/goshy.nix
+++ contrib/nixos/goshy.nix
@@ -1,39 +1,46 @@
 { config, lib, pkgs, ... }:
 
 with lib;
+
 let
   cfg = config.programs.goshy;
 
   goshy = pkgs.stdenv.mkDerivation rec {
     name = "goshy";
+
     src = ../../.;
+
     buildInputs = [ pkgs.bash pkgs.curl ];
     nativeBuildInputs = [ pkgs.makeWrapper ];
+
     installPhase = ''
       mkdir -p $out/bin
       cp ./contrib/bash/goshy $out/bin/goshy
       wrapProgram $out/bin/goshy \
-        --set GOSH_INSTANCE ${cfg.instance} \
-        --set PERIOD "-F time=${cfg.expiryPeriod}" \
-        ${lib.optionalString cfg.burnAfterReading "--set BURN '-F burn=1'"} \
-        ${lib.optionalString cfg.printOnlyUrl "--set ONLYURL '?onlyURL'"} \
+        --set GOSH_INSTANCE "${cfg.instance}" \
+        --set PERIOD "${cfg.expiryPeriod}" \
+        ${lib.optionalString cfg.burnAfterReading "--set BURN 1"} \
+        ${lib.optionalString cfg.printOnlyUrl "--set ONLYURL 1"}
     '';
   };
-
-
-in {
+in
+{
   options = {
     programs.goshy = {
       enable = mkEnableOption "Upload files to an instance of gosh! Go Share";
+
       instance = mkOption {
         type = types.str;
       };
+
       burnAfterReading = mkEnableOption "Enable burn after reading by default";
+
       expiryPeriod = mkOption {
         type = types.nullOr types.str;
         description = "Set a custom default expiry period, e.g., one minute";
         default = "3d";
       };
+
       printOnlyUrl = mkEnableOption "Enable only printing the resulting URL";
     };
   };
